ce6783e6f9525f9243702e1bfcd5ab5e0032c5b5
Martin G. Skjæveland <m.g.skjaeveland@gmail.com>
2017-11-08 17:07:18 +0100

    reducing complexity and duplication of shortname and typedname
    typedname is now a subclass of shortname.
    moving literal shortnaming to RuleUtils.

--- rdfvizler-core/src/main/java/osl/rdfvizler/dot/rules/ShortValue.java
+++ rdfvizler-core/src/main/java/osl/rdfvizler/dot/rules/ShortValue.java
@@ -1,7 +1,6 @@
 package osl.rdfvizler.dot.rules;
 
 import org.apache.jena.graph.Node;
-import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.reasoner.rulesys.BuiltinException;
 import org.apache.jena.reasoner.rulesys.RuleContext;
 
@@ -13,18 +12,10 @@ public class ShortValue extends NodeFunction {
     }
 
     protected Node value(Node node, RuleContext context) {
-        String string;
-        if (node.isBlank() || node.isURI()) {
-            string = RuleUtils.getShortForm(node, context);
-        } else if (node.isLiteral()) {
-            string = node.getLiteralLexicalForm();
-            String datattype = node.getLiteralDatatypeURI();
-            if (datattype != null && datattype.length() > 0) {
-                string += "^^" + RuleUtils.getShortForm(datattype, context);
-            }
-        } else {
+        if (! (node.isBlank() || node.isURI() || node.isLiteral())) {
             throw new BuiltinException(this, context, "Illegal node type: " + node);
-        }
-        return ResourceFactory.createPlainLiteral(string).asNode();
+        } 
+        String string = RuleUtils.getShortForm(node, context);
+        return RuleUtils.stringAsNode(string);
     }
 }
